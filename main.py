c9bb80ab624c1022703afdcddbe6c739d0f71a60
James Jones <james@edgeforge.com>
2022-03-16 15:21:44 +0000

    James did a stupid, vinnie was right

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request
+from guesslang import Guess
+import os
+
+app = Flask(__name__)
+guess = Guess()
+
+@app.route('/'+os.envrion['EF_NAME'])
+def hello():
+    return "Hello from Python!"
+
+@app.route('/'+os.envrion['EF_NAME'], methods = ['POST'])
+def make_guess():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        return json
+    elif (content_type == 'text/plain'):
+        sample = request.data
+        print(sample)
+        name = guess.language_name(sample)
+        print(name)
+        return(name)
+    else:
+        return 'Content-Type not supported!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+    
